ced645895d4f474cd068379382755a78953bc194
Jenny <jfperham@gmail.com>
2022-06-12 08:31:23 -0700

    added css

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import HeroSection from "./components/HeroSection";
+import LoginControls from "./components/LoginControls";
+import StatsBar from "./components/StatsBar";
+import SearchBar from "./components/SearchBar";
+import NewsletterForm from "./components/NewsletterForm";
+import NavigationBar from "./components/NavigationBar";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavigationBar></NavigationBar>
     </div>
   );
 }
---
+++ src/components/NavigationBar.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./NavigationBar.css";
+
+const NavigationBar = () => {
+  return (
+    <ul>
+      <li>Why Github?</li>
+      <li>Team</li>
+      <li>Enterprise</li>
+      <li>Explore</li>
+      <li>Marketplace</li>
+      <li>Pricing</li>
+    </ul>
+  );
+};
+
+export default NavigationBar;
